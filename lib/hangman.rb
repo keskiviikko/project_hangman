c2b7f6056182676706516b7e6ad247c40e701740
Rasmus Rajasalo <rarasmus@outlook.com>
2019-06-08 15:55:35 +0900

    Update methods

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -4,38 +4,37 @@ class Game
 
     def initialize
         @the_word = random_word.upcase
-        @player_input = String.new
+        @player_input = Array.new
         @attempts_left = 10
     end
 
     def random_word
-        @the_word = File.readlines("../5desk.txt").sample
+        @the_word = File.readlines("../5desk.txt").sample.strip()
     end
 
     def hide_the_word
-        
+        @hidden_word = "_" * @the_word.size
+        puts "Can you find out this word? #{@hidden_word}"
+        puts "You have #{@attempts_left} attempts left."
+        puts @the_word #delete this
     end
 
-    def update #change all of this
-        puts @hidden_word
-        puts @the_word
+    def update
+        if @the_word.chars.any? {|letter| @player_input.include?(letter.downcase)}
+            puts "updated hidden word" #change this
+        elsif
+            puts @hidden_word
+        end
+        puts "You have #{@attempts_left-1} attempts left."
     end
 
     def guess_a_letter
         @player_input << gets.chomp
         puts "All the letters you have guessed: #{@player_input}"
     end
-
-    def save
-
-    end
-
-    def load
-
-    end
     
     def has_won?
-        if @player_input.include?("w") #change this
+        if !@hidden_word.include?("_") || @player_input.include?(@the_word.downcase)
             puts "You won!"
         elsif @attempts_left == 0
             puts "You lost..."
@@ -43,6 +42,7 @@ class Game
     end
 
     def game_round
+        puts "Let's play hangman!"
         hide_the_word
         while @attempts_left > 0
             guess_a_letter
@@ -51,6 +51,14 @@ class Game
             has_won?
             break if @player_input.include?("q") #delete this
         end
+    end
+
+    def save
+
+    end
+
+    def load
+
     end
 
 end
