df0ba6c56ef5f75060e300a5b173a888cbe0829b
Rasmus Rajasalo <rarasmus@outlook.com>
2019-06-11 16:57:51 +0900

    Improve methods and fix the problems

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -4,7 +4,8 @@ class Game
 
     def initialize
         @the_word = random_word.upcase
-        @player_input = Array.new
+        @player_arr = Array.new
+        @correct_letters = Array.new
         @wrong_letters = Array.new
         @attempts_left = 10
     end
@@ -15,7 +16,6 @@ class Game
 
     def hide_the_word
         @hidden_word = "_" * @the_word.size
-        puts @the_word #delete this!
     end
 
     def explain_rules
@@ -24,45 +24,57 @@ class Game
         puts "To quit the game type: quitgame"
         puts
         puts "Can you find out this word? #{@hidden_word}"
-        puts "You have #{@attempts_left} attempts left."
+        puts "Attempts left: #{@attempts_left}/10"
+    end
+
+    def player_input
+        @player_arr << gets.chomp.upcase
+        if @player_arr[-1].empty? #false input
+            puts "Type any letter A-Z or a word."
+        elsif @player_arr.include?("QUITGAME") #player quits
+            puts "You gave up..."
+        elsif @correct_letters.include?(@player_arr[-1]) #correct guess
+            puts "You've already tried that..."
+        elsif @the_word.include?(@player_arr[-1]) #correct guess
+            puts "Nice!"
+        elsif @wrong_letters.include?(@player_arr[-1]) #wrong guess
+            puts "You've already tried that..."
+        else #wrong guess
+            @attempts_left -= 1
+            puts "That's not it!"
+        end
+    end
+
+    def update_attempts_left
+        puts "Attempts left: #{@attempts_left}/10"
+    end
+
+    def update_correct_letters
+        if @the_word.include?(@player_arr[-1])
+            @correct_letters << @player_arr[-1] unless @correct_letters.include?(@player_arr[-1])
+        end
+    end
+
+    def update_wrong_letters
+        if !@the_word.include?(@player_arr[-1])
+            @wrong_letters << @player_arr[-1] unless @wrong_letters.include?(@player_arr[-1])
+        end
+        puts "Wrong letters: #{@wrong_letters}"
     end
 
     def update_hidden_word
         if @the_word.chars.any? do |letter|
-            @player_input.include?(letter.upcase)
+            @player_arr.include?(letter.upcase)
             end
-            puts @hidden_word = @the_word.chars.map { |c| @player_input.include?(c) ? c : '_' }.join
+            puts @hidden_word = @the_word.chars.map { |c| @player_arr.include?(c) ? c : '_' }.join
         else
             puts @hidden_word
         end
     end
 
-    def update_player_input
-        @player_input << gets.chomp.upcase
-        if @player_input.include?("QUITGAME")
-            puts "You gave up..."
-        elsif @the_word.include?(@player_input[-1])
-            puts "You have #{@attempts_left} attempts left."
-        elsif @wrong_letters.include?(@player_input[-1])
-            puts "You've already tried that..."
-            puts "You have #{@attempts_left} attempts left."
-        else
-            @attempts_left -= 1
-            puts "That's not it!"
-            puts "You have #{@attempts_left} attempts left."
-        end
-    end
-
-    def update_wrong_letters
-        if !@the_word.include?(@player_input[-1])
-            @wrong_letters << @player_input[-1] unless @wrong_letters.include?(@player_input[-1])
-        end
-        puts "All the wrong letters you have guessed: #{@wrong_letters}"
-    end
-
     def has_won?
-        if !@hidden_word.include?("_") || @player_input.include?(@the_word.upcase)
-            puts "You won!"
+        if !@hidden_word.include?("_") || @player_arr.include?(@the_word.upcase)
+            puts "You won!!"
             puts "The word is #{@the_word}"
         elsif @attempts_left == 0
             puts "You lost..."
@@ -78,14 +90,16 @@ class Game
         while @attempts_left > 0
             puts
             print "Enter your guess: "
-            update_player_input
-            break if @player_input.include?("QUITGAME")
+            player_input
+            break if @player_arr.include?("QUITGAME")
+            update_attempts_left
+            update_correct_letters
             update_wrong_letters
+            break if @player_arr.include?(@the_word.upcase)
             update_hidden_word
-            has_won?
-            break if !@hidden_word.include?("_") ||
-                    @player_input.include?(@the_word.upcase)
+            break if !@hidden_word.include?("_")
         end
+        has_won?
     end
 
     def save
